691f1c1038398248a70e53956d704ba29d8bff10
unknown <prashantsingh7978@gmail.com>
2022-11-15 14:43:43 +0530

    fix textfit

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,14 +1,13 @@
 import { useContext } from "react"
 import { CalcContext } from "../context/CalcContext"
-import { Textfit } from 'react-textfit';
+// import { Textfit } from 'react-textfit';
 
 const Screen = () => {
   const { calc } = useContext(CalcContext);
 
   return (
-    <Textfit className="screen" max={70}  mode="single">{calc.num ? calc.num : calc.res}</Textfit>
+    <div  className="screen" max={70}  mode="single">{calc.num ? calc.num : calc.res}</div>
   )
 }
-
 
 export default Screen
